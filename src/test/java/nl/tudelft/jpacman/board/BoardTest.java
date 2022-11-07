4abf290f2c8d7c57487637f6530cefdc0252585e
Don <81351757+Don-NPT@users.noreply.github.com>
2022-11-07 11:10:33 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -3,34 +3,37 @@ package nl.tudelft.jpacman.board;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+// Test Board
 public class BoardTest {
     private Square[][] grid;
     private Board board;
 
     private final Square square = new BasicSquare();
 
+    // Test Basic Square
     @Test
-    void BasicSquare () {
-        grid = new Square[][]{{ square,square}};
+    void basicSquare() {
+        grid = new Square[][]{{ square, square}};
         board = new Board(grid);
         assertThat(board.invariant()).isEqualTo(true);
     }
 
+    // Test testSquareAt
     @Test
     void testSquareAt() {
         grid = new Square[][]{
-            {square,null}
+            {square, null}
         };
         board = new Board(grid);
         assertThat(board.squareAt(0, 0)).isEqualTo(square);
 
     }
 
+    // Test testSquareAt2
     @Test
     void testSquareAt2() {
         grid = new Square[][]{
-            {square,square}
+            {square, square}
         };
         board = new Board(grid);
         assertThat(board.squareAt(0, 0)).isEqualTo(square);
